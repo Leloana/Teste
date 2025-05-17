246a2423ba15c24f0653899fa636ffe1e7dccb68
Marcelo <mf8271977@gmail.com>
2025-05-17 18:31:42 -0300

    first commit

---
+++ meu-app-react/src/App.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import { supabase } from './supabaseClient'
+
+function App() {
+  const [dados, setDados] = useState<any[]>([])
+
+  useEffect(() => {
+    async function buscar() {
+      const { data, error } = await supabase.from('processos').select('*')
+      if (error) console.error('Erro:', error)
+      else setDados(data)
+    }
+
+    buscar()
+  }, [])
+
+  return (
+    <div>
+      <h1>Dados do Supabase</h1>
+      <ul>
+        {dados.map((item, index) => (
+          <li key={index}>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
